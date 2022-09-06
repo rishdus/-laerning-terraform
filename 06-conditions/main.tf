9ea995cf6ffafddccd17acca87ebc262b7d542ad
rishdus <risheek.dustin@gmail.com>
2022-09-06 18:06:24 +0530

    updating ./learning-terraform

--- 06-conditions/main.tf
+++ 06-conditions/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
   ami           = "ami-0f0d5d950bc2520f0"
-  instance_type = var.instancce_type == null ? "t3.micro" : var.instancce_type
+  instance_type = var.instance_type == null ? "t3.micro" : var.instance_type
 }
 
-variable "instancce_type" {
+variable "instance_type" {
   default = null
 }
