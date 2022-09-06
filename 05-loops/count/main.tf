6417af5a4fb6c80f197f14a9517116592446e907
rishdus <risheek.dustin@gmail.com>
2022-09-06 17:34:44 +0530

    updating ./learning-terraform

--- 05-loops/count/main.tf
+++ 05-loops/count/main.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "web" {
   ami           = "ami-0f0d5d950bc2520f0"
   instance_type = "t3.micro"
   tags = {
-    Name = "sample-${count.index}"
+    Name = "sample-${count.index + 1}"
   }
 }
