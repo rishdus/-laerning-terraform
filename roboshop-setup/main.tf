e7db95ce3897e0eab045264a4f9d82890c81c424
rishdus <risheek.dustin@gmail.com>
2022-09-09 16:29:08 +0530

    updating ./learning-terraform

--- roboshop-setup/main.tf
+++ roboshop-setup/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "app" {
   count = length(var.components)
   ami   = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
+  iam_instance_profile = "SecretManager_Role_For_RoboShop_Nodes"
   tags = {
     Name = "${var.components["${count.index}"]}-dev"
   }
@@ -14,4 +15,21 @@ resource "aws_route53_record" "record" {
   type    = "A"
   ttl     = 30
   records = [aws_instance.app.*.private_ip[count.index]]
+}
+
+resource "null_resource" "ansible-apply" {
+  count = length(var.components)
+  provisioner "remote-exec" {
+
+    connection {
+      host = aws_instance.app*.public_ip[count.index]
+      user = "root"
+      password = "DevOps321"
+    }
+
+    inline = [
+      "ansible-pull -i localhost, -U https://github.com/rishdus/roboshop-mutable-ansible roboshop.yml -e HOSTS=localhost -e APP_COMPONENT_ROLE=${var.components["${count.index}"]} -e ENV=dev"
+    ]
+
+  }
 }
