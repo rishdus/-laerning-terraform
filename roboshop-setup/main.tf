9be5d22559cde8e52c56dc1e7e7b742d7f2e71a9
rishdus <risheek.dustin@gmail.com>
2022-09-09 17:23:27 +0530

    updating ./learning-terraform

--- roboshop-setup/main.tf
+++ roboshop-setup/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "app" {
-  count = length(var.components)
-  ami   = data.aws_ami.ami.image_id
-  instance_type = "t3.micro"
-  iam_instance_profile = "SecretManager_Role_For_RoboShop_Nodes"
+  count                  = length(var.components)
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = "t3.micro"
+  iam_instance_profile   = "SecretManager_Role_For_RoboShop_Nodes"
   vpc_security_group_ids = ["sg-04af99788d75b3da0"]
   tags = {
     Name = "${var.components["${count.index}"]}-dev"
@@ -10,7 +10,7 @@ resource "aws_instance" "app" {
 }
 
 resource "aws_route53_record" "record" {
-  count = length(var.components)
+  count   = length(var.components)
   zone_id = "Z095677034JZB3ZMFYPOS"
   name    = "${var.components["${count.index}"]}-dev"
   type    = "A"
@@ -19,6 +19,7 @@ resource "aws_route53_record" "record" {
 }
 
 resource "null_resource" "ansible-apply" {
+  depends_on = [aws_route53_record.record]
   triggers = {
     abc = timestamp()
   }
@@ -26,8 +27,8 @@ resource "null_resource" "ansible-apply" {
   provisioner "remote-exec" {
 
     connection {
-      host = aws_instance.app.*.public_ip[count.index]
-      user = "root"
+      host     = aws_instance.app.*.public_ip[count.index]
+      user     = "root"
       password = "DevOps321"
     }
 
