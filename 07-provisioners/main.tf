d2897ac0cb2dad9f5c05cb11c0d8a9db77004c17
rishdus <risheek.dustin@gmail.com>
2022-09-09 15:17:52 +0530

    updating ./learning-terraform

--- 07-provisioners/main.tf
+++ 07-provisioners/main.tf
@@ -1,16 +1,37 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0f0d5d950bc2520f0"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-04af99788d75b3da0"]
+## resource "aws_instance" "web" {
+##   ami           = "ami-0f0d5d950bc2520f0"
+##   instance_type = "t3.micro"
+##   vpc_security_group_ids = ["sg-04af99788d75b3da0"]
+##
+##   provisioner "remote-exec" {
+##     connection {
+##       host = self.public_ip
+##       user = "root"
+##       password = "DevOps321"
+##           }
+##     inline = [
+##       "echo Hello World"
+##     ]
+##   }
+## }
 
+resource "aws_instance" "web" {
+  ami                    = "ami-0f0d5d950bc2520f0"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-04af99788d75b3da0"]
+}
+
+resource "null_resource" "cli" {
   provisioner "remote-exec" {
     connection {
-      host = self.public_ip
-      user = "root"
+      host     = aws_instance.web.public_ip
+      user     = "root"
       password = "DevOps321"
-          }
-    inline = [
-      "echo Hello World"
+    }
+    inline =  [
+    "echo Hello World "
     ]
+    
   }
+
 }
