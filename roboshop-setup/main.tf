2fca62da06c2605db3e8ad64e26f0434fadeed05
rishdus <risheek.dustin@gmail.com>
2022-09-09 16:02:33 +0530

    updating ./learning-terraform

--- roboshop-setup/main.tf
+++ roboshop-setup/main.tf
@@ -1,5 +1,8 @@
 resource "aws_instance" "app" {
   count = length(var.components)
-  ami = data.aws_ami.ami.image_id
+  ami   = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
+  tags = {
+    Name = "${var.components["${count.index}"]}-dev"
+  }
 }
